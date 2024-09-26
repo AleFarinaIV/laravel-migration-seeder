75bb837e77f13255cde3488865bf007f7bfa152c
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-09-26 15:54:54 +0200

    [UPD] created table's columns and model and seeder php files

--- database/migrations/2024_09_26_133327_create_trains_table.php
+++ database/migrations/2024_09_26_133327_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 30);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno', 50);
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
